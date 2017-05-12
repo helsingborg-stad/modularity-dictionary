2d46c307ddadab5fe909f2192cef39bf7f69f44e
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-05-12 14:21:57 +0200

    Add module in hook.

--- source/php/App.php
+++ source/php/App.php
@@ -7,7 +7,10 @@ class App
     public function __construct()
     {
         new \ModularityDictionary\Dictionary();
-        new \ModularityDictionary\Module();
+
+        add_action('Modularity', function () {
+            new \ModularityDictionary\Module();
+        });
 
         add_filter('acf/settings/load_json', array($this, 'jsonLoadPath'));
     }
